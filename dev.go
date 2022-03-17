8155e5204bfce4ed56be280c35e936e7dad0dcb7
Zach Chan <564737735@qq.com>
2022-03-17 20:52:23 +0800

    青龙 备份

---
+++ dev.go
@@ -0,0 +1,17 @@
+package main
+import (
+//青龙2.9机器人
+_ "github.com/cdle/sillyGirl/develop/qinglong" 
+//京东账号
+_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+//腾讯QQ
+_ "github.com/cdle/sillyGirl/im/qq"
+//Te Legram
+_ "github.com/cdle/sillyGirl/im/tg"
+)
+//京东资产 查询
+//_"github.com/cdle/sillyGirl/develop/jd_asset"
+//京东互助
+//_"github.com/cdle/sillyGir/develop/jd_help"
+//jd_ wskey转换
+//_"github.com/cdle/sillyGirl/develop/jd_wskey"
